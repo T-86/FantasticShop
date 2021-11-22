951d9083e74c8de6cb43d4566e1b0f7013b74787
T-86 <sangodbz@yahoo.fr>
2021-11-22 16:19:02 +0000

    updated the yellow screen to load when open updated the CreateAccountActivity and the userLoginActivity with email and password validation

--- app/src/main/java/com/example/fantasticshop/HomeActivity.java
+++ app/src/main/java/com/example/fantasticshop/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.fantasticshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.fantasticshop.fragments.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // fragment injection
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new HomeFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/fantasticshop/fragments/VerticalProducts.java
@@ -0,0 +1,5 @@
+package com.example.fantasticshop.fragments;
+
+public class VerticalProducts {
+
+}
